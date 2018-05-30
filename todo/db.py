bcc7019d04f4e87799312d024221bd1258b66c34
junhao1996 <1258884157@qq.com>
2018-05-30 21:05:22 +0800

    0006 delete button

--- todo/db.py
+++ todo/db.py
@@ -5,14 +5,27 @@ class TodoDB():
     def read_all(self):
         conn = sqlite3.connect('test.db')
         cursor = conn.cursor()
-        cursor.execute("select * from todo")
+        cursor.execute("select * from todo order by id desc")
         res = cursor.fetchall()
-        result = [(i[0],i[1]) for i in res]
-        return result
+        # result = [(i[0],i[1]) for i in res]
+        return res
+    def read_one(self):
+        conn = sqlite3.connect('test.db')
+        cursor = conn.cursor()
+        cursor.execute("select * from todo order by id desc limit 0,1")
+        res = cursor.fetchall()
+        return res
+    def read_kongque(self):
+        conn = sqlite3.connect('test.db')
+        cursor = conn.cursor()
+        cursor.execute("select * from todo order by id desc limit 0,1")
+        res = cursor.fetchall()
+        return res
     def init__db(self):
         conn = sqlite3.connect('test.db')
         cursor = conn.cursor()
-        # cursor.execute("create table todo(id integer primary key autoincrement ,content varchar(200))")
+        # cursor.execute("drop table if exists todo")
+        cursor.execute("create table todo(id integer primary key autoincrement ,content varchar(200))")
 
         conn.commit()
     def add(self):
@@ -22,9 +35,19 @@ class TodoDB():
         cursor.close()
         conn.commit()
         conn.close()
+    def delete(self):
+        conn = sqlite3.connect('test.db')
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM todo WHERE id = 1;")
+        cursor.close()
+        conn.commit()
+        conn.close()
 if __name__ =="__main__":
     todo = TodoDB()
     # todo.init__db()
     todo.add()
+    # todo.delete()
     a= todo.read_all()
+    b = todo.read_one()
+    print(b)
     print(a)
